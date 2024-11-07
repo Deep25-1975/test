c3bb13769ab5f1cef9bc6b85cb0fce205ae99a05
Deep25-1975 <deep25.1975@gmail.com>
2024-11-07 21:53:15 +0530

    Revert "added 2 new files and a print statement"
    This reverts commit 75ff29716325266300482d860c7300ca6d2f71fe.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,5 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "My name is Deepika" << std::endl;
-    std::cout << "My name is Roosh" << std::endl;
     return 1;
 }
