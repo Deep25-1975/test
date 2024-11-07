75ff29716325266300482d860c7300ca6d2f71fe
Deep25-1975 <deep25.1975@gmail.com>
2024-11-07 21:52:35 +0530

    added 2 new files and a print statement

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "My name is Deepika" << std::endl;
+    std::cout << "My name is Roosh" << std::endl;
     return 1;
 }
