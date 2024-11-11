89154d8976c44c98def9845e424d822cf0d9542a
Deep25-1975 <deep25.1975@gmail.com>
2024-11-11 14:18:23 +0530

    two new print lines

---
+++ main1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello,1234" << std::endl;
+    std ::cout << "version control" << std::endl;
+    return 0;
+}
